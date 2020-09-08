396c1e78226a2ad16753205381f099cb253a212d
Tolshao <zhanghao3440@gmail.com>
2020-09-08 14:53:47 +0800

    update gym_test.py

--- Mygym/gym_test.py
+++ Mygym/gym_test.py
@@ -1,17 +1,33 @@
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
-
+plt.style.use(['science'])
 
 env = gym.make('AC_brake-v0')
 
 env.reset()
 
+data_list = []
+endtime = 0
 for i in range(300):
     obs, reward, done, _ = env.step(1)
-    data = [obs, reward, done]
+    data_list.append([obs, reward, done])
     # print(obs)
     if done:
-        print(i)
+        print('The episode ended at {} steps'.format(i + 1))
+        endtime = i
         break
-print(int(obs[1]), reward)
+
+
+# plot data_list
+
+time = np.arange(endtime+1)
+data_list = np.array(data_list)
+print(np.shape(data_list))
+yy = []
+for data in data_list[:, 0]:
+    yy.append(data[0])
+print('a')
+print('b')
+plt.plot(time, yy)
+plt.show()
