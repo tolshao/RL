b215f892c3ac22b9efc7ce8f1b32c0a37798c981
Tolshao <zhanghao3440@gmail.com>
2020-09-08 17:37:27 +0800

    change the folder directory

--- Mygym_env/ac_brake_test.py
+++ Mygym_env/ac_brake_test.py
@@ -23,10 +23,10 @@ nb_actions = env.action_space.n
 model = Sequential()
 model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
 model.add(Dense(16))
-#model.add(Activation('relu'))
-#model.add(Dense(16))
-#model.add(Activation('relu'))
-#model.add(Dense(16))
+# model.add(Activation('relu'))
+# model.add(Dense(16))
+# model.add(Activation('relu'))
+# model.add(Dense(16))
 model.add(Activation('relu'))
 model.add(Dense(nb_actions))
 model.add(Activation('linear'))
@@ -38,19 +38,20 @@ memory = SequentialMemory(limit=50000, window_length=1)
 
 policy = BoltzmannQPolicy()
 
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10, target_model_update=1e-2, policy=policy)
-               
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory,
+               nb_steps_warmup=10, target_model_update=1e-2, policy=policy)
+
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-history = dqn.fit(env, nb_steps=2000, visualize=False, verbose=2,callbacks=[WandbLogger()])
+history = dqn.fit(env, nb_steps=2000, visualize=False,
+                  verbose=2, callbacks=[WandbLogger()])
 
 # After training is done, we save the final weights.
-dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+dqn.save_weights('weights/dqn_{}_weights.h5f'.format(
+    ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
 history_test = dqn.test(env, nb_episodes=5, visualize=False)
-
-
