8547265e2c47ea3acfe2b4c9b49e55404125d337
Tolshao <zhanghao3440@gmail.com>
2020-09-11 17:11:22 +0800

    finish Int_0 Aircraft model

--- Mygym_env/AC_Maneuver/Aircraft.py
+++ Mygym_env/AC_Maneuver/Aircraft.py
@@ -7,22 +7,23 @@
 import numpy as np
 
 
-class Aircract(object):
-    def __init__(self, states0, Tsc=0.02):
+class Aircraft(object):
+    def __init__(self, states0: float, Tsc=0.02, RK4=False):
         # Aircraft parameters
         self.mass = 9259.44
         Ixx = 12874.8
         Iyy = 75673.6
         Izz = 85552.1
         Ixz = 1331.4
-        Ixy = 0
-        Iyz = 0
+        Ixy = 0.0
+        Iyz = 0.0
         self.inertia = np.matrix(
             [[Ixx, -Ixy, -Ixz], [-Ixy, Iyy, -Iyz], [-Ixz, -Iyz, Izz]])
         self.Inv_inertia = self.inertia.I
 
         # Env paprameters
         self.Tsc = Tsc
+        self.RK4 = RK4
 
         # State variables of C.G. of AC
         x, vel, euler, ang_vel = states0
@@ -30,41 +31,45 @@ class Aircract(object):
         self.vel = np.matrix(vel)
         self.euler = np.matrix(euler)
         self.ang_vel = np.matrix(ang_vel)
-        self.forces = np.zeros((1, 3))
-        self.moments = np.zeros((1.3))
+        self.forces = np.matrix([0, 0, 0], dtype=float)
+        self.moments = np.matrix([0, 0, 0], dtype=float)
         self.dcm = np.matrix(np.eye(3, dtype=float))
 
     def update_onestep(self, forces, moments):
-        print("the input is forces{}, moments{}".format(forces, moments))
+        # test print
+        # print("the input is forces{}, moments{}".format(forces, moments))
         # get the input variables
+        self.forces = np.matrix(forces)
+        self.moments = np.matrix(moments)
 
-        # 计算各个向量的微分
-        dot_vel = self.DotVel_get(forces)
-        dot_ang_vel = self.DotAngVel_get(moments)
-        dot_x = self.Vel_g_get()
-        dot_euler = self.DotEuler_get()
-        # 将变量积分
-        self.vel = self.Integrator(self.vel, dot_vel)
-        self.ang_vel = self.Integrator(self.ang_vel, dot_ang_vel)
-        self.x = self.Integrator(self.x, dot_x)
-        self.euler = self.Integrator(self.euler, dot_euler)
-        self.DCM_get()
+        if self.RK4 == False:
+            self.Int_0()
+        else:
+            self.IntRK4()
 
-    def Integrator(self, input_X, input_dot_X):
-        X = np.matrix(input_X)
-        dot_X = np.matrix(input_dot_X)
-        assert X.shape == dot_X.shape, 'the dimension is different'
-        return X + self.Tsc * dot_X
+    def Int_0(self):
+        # 刷新坐标系转换矩阵
+        self.DCM_refresh()
+        # 计算各个向量的微分 并 积分
+        self.vel += self.DotVel_get() * self.Tsc
+        self.ang_vel += self.DotAngVel_get() * self.Tsc
+        self.x += self.Vel_g_get() * self.Tsc
+        self.euler += self.DotEuler_get() * self.Tsc
 
-    def DotAngVel_get(self, input_moments):
-        moments = np.matrix(input_moments)
+    def IntRK4(self):
+        return False
+
+    # def RK4ForOne(self,Func,):
+
+    def DotAngVel_get(self):
+        moments = self.moments
         assert moments.shape == self.ang_vel.shape, 'the dimension is different'
         Iw = np.matmul(self.inertia, self.ang_vel.T)
         w_x_Iw = np.cross(self.ang_vel, Iw.T)
         return np.matmul(self.Inv_inertia, (moments - w_x_Iw).T).T
 
-    def DotVel_get(self, input_forces):
-        forces = np.matrix(input_forces)
+    def DotVel_get(self):
+        forces = self.forces
         assert forces.shape == self.ang_vel.shape, 'the dimension is different'
         assert forces.shape == self.vel.shape, 'the dimension is different'
         v_x_w = np.cross(self.vel, self.ang_vel)
@@ -80,7 +85,7 @@ class Aircract(object):
         out3 = u2*np.sin(u4)/np.cos(u5)+u3*np.cos(u4)/np.cos(u5)
         return np.matrix([out1, out2, out3])
 
-    def DCM_get(self):
+    def DCM_refresh(self):
         u1, u2, u3 = self.euler[0, 0], self.euler[0, 1], self.euler[0, 2]
         # test print
         # print(u1, u2, u3, type(u1), type(u2), type(u3))
@@ -108,28 +113,24 @@ def test():
     Tsc = 0.01
     print("testing----------------------------------------------------------------------------------------------------")
 
-    x0 = [0, 0, 0]
+    x0 = [[0.0, 0.0, 0.0]]
     vel0 = [1.0, 2.0, 3.0]
     euler0 = [0.1, 0.2, 0.3]
     ang_vel0 = [0.1, 0.2, 0.3]
 
     initial_parameters = [x0, vel0, euler0, ang_vel0]
-    ac = Aircract(initial_parameters, Tsc=Tsc)
+    ac = Aircraft(initial_parameters, Tsc=Tsc)
 
     # print("inv_inertia is \n{}".format(ac.Inv_inertia))
 
-    dot_x_temp = [0, 0, 1]
-    temp = ac.Integrator(ac.x, dot_x_temp)
-    print(temp, type(temp), temp.shape)
-    print("1. Func Integrator success")
-    moments = [3, 2, 1]
+    ac.Int_0()
+    print("1. Func Int_0 success")
 
-    temp = ac.DotAngVel_get(moments)
+    temp = ac.DotAngVel_get()
     print(temp, type(temp), temp.shape)
     print("2. Func DotAngVel_get success",)
 
-    forces = [1, 2, 3]
-    temp = ac.DotVel_get(forces)
+    temp = ac.DotVel_get()
     print(temp, type(temp), temp.shape)
     print("3. Func DotVel_get success")
 
@@ -137,13 +138,18 @@ def test():
     print(temp, type(temp), temp.shape)
     print("4. Func DotEuler_get success")
 
-    temp = ac.DCM_get()
+    temp = ac.DCM_refresh()
     print(temp, type(temp), temp.shape)
-    print("5.Func DCM_get success")
+    print("5. Func DCM_refresh success")
 
     temp = ac.Vel_g_get()
     print(temp, type(temp), temp.shape)
-    print("6.Func Vel_get success")
+    print("6. Func Vel_get success")
+
+    moments = [1.0, 2.0, 3.0]
+    forces = [1.0, 2.0, 3.0]
+    ac.update_onestep(forces, moments)
+    print("7. Func update_onestep success")
 
 
 if __name__ == "__main__":
--- Mygym_env/AC_Maneuver/test.py
+++ Mygym_env/AC_Maneuver/test.py
@@ -1,36 +1,29 @@
-from six_dof_toolbox import Aircract
+from Aircraft import Aircraft
 import numpy as np
 
 time_step = 0.01
 
-def main():
+def test():
 	
-	print("testing")
+	print("testing----------------------------------------------------")
 	
-	x0 = [0, 0, 0]
+	x0 = [0.0, 0.0, 0.0]
 	vel0 = [1.0, 2.0, 3.0]
 	euler0 = [0.1, 0.2, 0.3]
 	ang_vel0 = [0.1, 0.2, 0.3]
 
 	initial_parameters = [x0, vel0, euler0, ang_vel0]
-	ac = Aircract(initial_parameters,Tsc = 0.01)
-	
-	# testing----------------
-	print(ac.vel)
-	vel = ac.Vel_g_get()
-	print(vel)
-	
+	ac = Aircraft(initial_parameters,Tsc = 0.01,RK4 = False)
 	forces = [1,1,1]
-	print(type(forces))
-	moments = [1,1,1]
-	
-	print(ac.vel,"before")
-	print(ac.ang_vel,"before")
+	forces = [0,0,0]
+	moments = [0,0,0]
+	print(ac.RK4)
+	print(ac.vel,ac.ang_vel)
+	print(ac.DotAngVel_get())
 	ac.update_onestep(forces, moments)
-	print(ac.vel)
-	print(ac.ang_vel)
+	print(ac.vel,ac.ang_vel)
 	
 	
 	
 if __name__ == "__main__":
-	main()
+	test()
