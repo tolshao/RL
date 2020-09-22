38872930567ca981686fedbe7c562acbdc6060ef
Tolshao <zhanghao3440@gmail.com>
2020-09-22 23:27:06 +0800

    LG func 8 success

--- Mygym_env/AC_Maneuver/LG.py
+++ Mygym_env/AC_Maneuver/LG.py
@@ -5,9 +5,11 @@
 # 2020-09-11 17:18:49
 
 import numpy as np
+import math
 
 
 class LandingGear(object):
+    # new the class LG object
     def __init__(self, Tsc=0.02, RK4=False):
         # Env paprameters
         self.Tsc = Tsc
@@ -22,23 +24,81 @@ class LandingGear(object):
 
         # State variables of LG
         self.x = np.matrix(np.zeros((1, 3)))
+        self.rotzmatrix = np.matrix(np.eye(3, dtype=float))
+        self.k_alpha = 1.0
+        self.F_z = 150000.0
 
-    def AC2LG(self, vel, ang_vel, x, dcm):
+    # position from ac coordinate to LG cooordinate
+    def Cal_vel_AC2LG(self, vel, ang_vel, x, dcm):
+        # shape check
         assert vel.shape == (1, 3), 'dimension error'
         assert ang_vel.shape == (1, 3), 'dimension error'
         assert x.shape == (1, 3), 'dimension error'
         assert dcm.shape == (3, 3), 'dimension error'
         self.vel = np.cross(ang_vel, self.pos_vec) + vel
         self.x = (np.matmul(dcm.T, self.pos_vec.T)).T + x
+        return self.vel, self.x
+
+    # calculate the forces on the tire
+    def Cal_SideSlipAngle(self):
+        vel_temp = self.vel
+        if vel_temp[0, 0] == 0:
+            self.alpha = np.sign(vel_temp[0, 1]) * np.pi * 0.5
+        else:
+            self.alpha = math.atan2(
+                vel_temp[0, 1], vel_temp[0, 0])
+        return self.alpha
+
+    def Force_x(self):
+        return 0.0
+
+    def Forces(self):
+        ans = np.matrix([self.Force_x(), self.Cal_SideSlipAngle()
+                         * self.k_alpha * self.F_z, self.F_z])
+        self.forces = ans
+        return ans
+
+    def Moments(self):
+        ans = np.cross(self.pos_vec, self.forces)
+        return ans
 
 
 class NoseLandingGear(LandingGear):
+    # new the NLG object inherit from LG
     def __init__(self, Tsc=0.02, RK4=False):
         super().__init__(Tsc, RK4)
         self.pos_vec = np.matrix([8.5, 0.0, 2.0])
+        self.delta = 0.0
+
+    def Cal_SideSlipAngle(self, delta):
+        self.delta = delta
+        vel_temp = self.vel
+        if vel_temp[0, 0] == 0:
+            self.alpha = np.sign(vel_temp[0, 1]) * np.pi * 0.5 - self.delta
+        else:
+            self.alpha = math.atan2(
+                vel_temp[0, 1], vel_temp[0, 0]) - self.delta
+        return self.alpha
+
+    def RotZ(self):
+        ans = np.matrix([[math.cos(self.alpha), -math.sin(self.alpha), 0],
+                         [math.sin(self.alpha), math.cos(self.alpha), 0], [0, 0, 1]])
+        return ans
+
+    def Forces(self):
+        ans = np.matrix([self.Force_x(), self.Cal_SideSlipAngle(self.delta)
+                         * self.k_alpha * self.F_z, self.F_z])
+        self.rotzmatrix = self.RotZ()
+        assert self.rotzmatrix.shape == (3, 3), "matrix shape error"
+        assert ans.T.shape == (3, 1), "matrix shape erroe"
+
+        self.forces = np.matmul(self.rotzmatrix, ans.T).T
+        return ans
 
 
 class MainLandingGear(LandingGear):
+    # new the MLG object inherit from LG
+    # 根据'L','R'决定位矢
     def __init__(self, direction, Tsc=0.02, RK4=False):
         super().__init__(Tsc, RK4)
         if direction == 'L':
@@ -50,6 +110,7 @@ class MainLandingGear(LandingGear):
 
 
 def test():
+    # 测试函数
     Tsc = 0.01
     print("testing----------------------------------------------------------------------------------------------------")
 
@@ -60,10 +121,10 @@ def test():
     DCM_tmp = np.matrix(np.eye(3, dtype=float))
     print(vel_tmp, ang_vel_tmp, x_tmp, DCM_tmp)
 
-    LG.AC2LG(vel_tmp, ang_vel_tmp, x_tmp, DCM_tmp)
+    LG.Cal_vel_AC2LG(vel_tmp, ang_vel_tmp, x_tmp, DCM_tmp)
     print(type(LG.vel),  type(LG.x))
     print(LG.vel.shape,  LG.x.shape)
-    print("1. Func AC2LG success")
+    print("1. Func Cal_vel_AC2LG success")
 
     NLG = NoseLandingGear(Tsc=Tsc)
     print(NLG.pos_vec)
@@ -76,6 +137,31 @@ def test():
     RMLG = MainLandingGear('R')
     print(RMLG.pos_vec)
     print("4. inherit RMLG success")
+
+    temp = np.matrix([1, 1, 1])
+    temp_0 = np.matrix([0, 0, 0])
+    a, b = NLG.Cal_vel_AC2LG(temp, temp / 1000, temp_0, DCM_tmp)
+    LMLG.Cal_vel_AC2LG(temp, temp / 1000, temp_0, DCM_tmp)
+    RMLG.Cal_vel_AC2LG(temp, temp / 1000, temp_0, DCM_tmp)
+    print("vel is {}, angular vel is {}".format(a, b))
+    print("5. Func Cal_vel_AC2LG success")
+
+    temp0 = NLG.Cal_SideSlipAngle(0)
+    temp1 = LMLG.Cal_SideSlipAngle()
+    temp2 = RMLG.Cal_SideSlipAngle()
+    print("Alpha are {},{},{}".format(temp0, temp1, temp2))
+    print("6.Func Cal_SideSlipAngle success")
+
+    NLG.alpha = math.pi / 2
+    ans = NLG.RotZ()
+    print(ans)
+    print("7. Func RotZ success")
+
+    ans = NLG.Forces()
+    print(ans)
+    ans = LMLG.Forces()
+    print(ans)
+    print("8. Func Forces success")
 
 
 if __name__ == "__main__":
